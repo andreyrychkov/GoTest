cd58c74499750c92c19dbc7f624ce6dc7155cb9f
Andrey Rychkov <andrey.rychkov@gmail.com>
2018-10-10 01:01:21 +0300

    upload added

--- GoTest/main.go
+++ GoTest/main.go
@@ -1,9 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"crypto/md5"
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+func savePostHandler(w http.ResponseWriter, r *http.Request) {
+	//id := r.FormValue("id")
+	//	title := r.FormValue("title")
+	//	content := r.FormValue("content")
+
+	http.Redirect(w, r, "/", 302)
+}
+
+// upload logic
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("upload.gtpl")
+		t.Execute(w, token)
+	} else {
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Fprintf(w, "%v", handler.Header)
+		f, err := os.OpenFile(handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
+	}
+}
 
 func main() {
 
 	fmt.Println("Listening port :8080")
+	http.HandleFunc("/SavePost", savePostHandler)
+	http.HandleFunc("/upload", upload)
 
+	http.ListenAndServe(":8080", nil)
 }
